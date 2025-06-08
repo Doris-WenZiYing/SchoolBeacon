0f8c0820400727e837cc8dd2b3de34317434e63c
doris <doris070714@gmail.com>
2025-06-08 16:16:47 +0800

    Initial Commit

---
+++ SchoolBeacon/SchoolBeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolBeaconApp.swift
+//  SchoolBeacon
+//
+//  Created by Doris Wen on 2025/6/8.
+//
+
+import SwiftUI
+
+@main
+struct SchoolBeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
